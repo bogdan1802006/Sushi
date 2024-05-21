ec48673e5105db8a6a8f61bd9f16e4e6f760b6d6
Bogdan Saipov <bogdansaipov18@gmail.com>
2024-05-21 18:35:20 +0500

    new sec

--- Js/script.js
+++ Js/script.js
@@ -16,11 +16,24 @@ btnMoon.addEventListener('click', function(){
     body.classList.toggle('dark-theme')
 })
 
-const cards = document.querySelectorAll('.popular-foods__card')
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.popular-foods__card');
 
-cards.forEach(card => {
-    card.addEventListener('click',function(){
-        cards.forEach(c => c.classList.remove('active-card'))
-        card.classList.add('active-card')
-    })
-})
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            // Remove 'active' class from all cards
+            cards.forEach(c => c.classList.remove('active-card'));
+            
+            // Add 'active' class to the clicked card
+            card.classList.add('active-card');
+        });
+    });
+});
+
+const contContainer = document.getElementsByClassName('content-container');
+
+for(let i = 0; i < contContainer.length; i++){
+    contContainer[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+    });
+}
